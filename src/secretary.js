00e9b9fb0341be60d2699d9743e0dbda63ef5b27
josuellions <josuel_lions@gmail.com>
2026-06-03 22:47:05 -0300

    feat: creates the function call in `google genAI`.

--- src/secretary.js
+++ src/secretary.js
@@ -1,10 +1,9 @@
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GEN_AI_API_KEY });
+try {
+  const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GEN_AI_API_KEY });
 
-const response = await ai.models.generateContent({
-  model: "gemini-2.5-flash",
-  contents: [
+  const contents = [
     {
       role: "user",
       parts: [
@@ -13,7 +12,49 @@ const response = await ai.models.generateContent({
         },
       ],
     },
-  ],
-});
+  ];
 
-console.log(response.candidates[0].content.parts);
+  let response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: contents,
+    config: {
+      tools: [
+        {
+          functionDeclarations: [
+            {
+              name: "getTodayDate",
+              description: "retorna a data de hoje no formato yyyy-mm-dd",
+            },
+          ],
+        },
+      ],
+    },
+  });
+
+  contents.push(response.candidates[0].content);
+
+  contents.push({
+    role: "user",
+    parts: [
+      {
+        functionResponse: {
+          name: "getTodayDate",
+          response: { result: "2026-06-03" },
+        },
+      },
+    ],
+  });
+
+  response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: contents,
+  });
+
+  console.log(response.candidates[0].content);
+} catch (error) {
+  if (error.status === 429) {
+    console.error("Limite de requisições excedido.");
+  } else {
+    console.error(error);
+  }
+}
